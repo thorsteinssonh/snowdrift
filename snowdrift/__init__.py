479449a2b7032ac487b5c1f3bfde11c86f80d659
Hrobjartur Thorsteinsson <thorsteinssonh>
2021-03-26 20:55:59 +0000

    Adds a few initial tests of library methods
    Changes to be committed:
    	modified:   snowdrift/__init__.py
    	modified:   tests/test.py

--- snowdrift/__init__.py
+++ snowdrift/__init__.py
@@ -1,4 +1,4 @@
-from .collect_data import collectData
-from .snowdrift import snowdrift
+from .collect_data import collectData, summary
+from .snowdrift import snowdrift, calculateDeps
 from .plot import plot
 from .save_grib import save_grib
--- tests/test.py
+++ tests/test.py
@@ -12,16 +12,15 @@ def get_data():
         # extract the zipped data
         os.system("cd tests; unzip testdata.zip")
         
-class TestForecast(unittest.TestCase):
+class Test(unittest.TestCase):
     @classmethod
     def setUpClass(self):
         # get_data will get test data if 'testdata' dir
         # is not found in test/ dir
         get_data()
-
-    def setUp(self):
-        # load testdata...git
-        files = ['tests/testdata/harmonie_2021032306.00', 
+        # load full testdata set
+        self.files = [
+            'tests/testdata/harmonie_2021032306.00', 
             'tests/testdata/harmonie_2021032306.03',
             'tests/testdata/harmonie_2021032306.06',
             'tests/testdata/harmonie_2021032306.01',
@@ -29,11 +28,60 @@ class TestForecast(unittest.TestCase):
             'tests/testdata/harmonie_2021032306.02',
             'tests/testdata/harmonie_2021032306.05'
         ]
-        self.data = snowdrift.collectData(files)
+        self.files3 = [
+            'tests/testdata/harmonie_2021032306.00',
+            'tests/testdata/harmonie_2021032306.01',
+            'tests/testdata/harmonie_2021032306.02',
+        ]
+        self.data, _ = snowdrift.collectData(self.files)
 
+
+    def setUp(self):
+        pass
 
     def tearDown(self):
         pass
 
-    def test_01(self):
-        pass
+    def test_input_data_params(self):
+        # check that we have loaded parameters:
+        # temp, snowac, snowground, temp, wind-u/v
+        params = sorted(self.data.keys())
+        self.assertEqual(params, ['snowac', 'snowground', 'temp', 'wind-u', 'wind-v'])
+
+    def test_calculate_deps(self):
+        # caculateDeps should add wind,
+        # and delete u/v vectors
+        data, _ = snowdrift.collectData(self.files3)
+        snowdrift.calculateDeps(data)
+        self.assertIn('wind', data)
+        self.assertNotIn('wind-u', data)
+        self.assertNotIn('wind-v', data)
+
+    def test_snowdrift(self):
+        # snowdrift algorithm should add,
+        # snowage, mobility, driftacc, drift parameters,
+        data, _ = snowdrift.collectData(self.files3)
+        snowdrift.snowdrift(data)
+        self.assertIn('snowage', data)
+        self.assertIn('mobility', data)
+        self.assertIn('driftacc', data)
+        self.assertIn('drift', data)
+
+    def test_input_data_sorting(self):
+        # order of forecast data input should not affect
+        # data loading...
+        files1 = [
+            'tests/testdata/harmonie_2021032306.00',
+            'tests/testdata/harmonie_2021032306.01',
+            'tests/testdata/harmonie_2021032306.02',
+        ]
+        files2 = [
+            'tests/testdata/harmonie_2021032306.01',
+            'tests/testdata/harmonie_2021032306.00',
+            'tests/testdata/harmonie_2021032306.02',
+        ]
+        data1, _ = snowdrift.collectData(files1)
+        data2, _ = snowdrift.collectData(files2)
+        times1 = data1['temp']['times']
+        times2 = data2['temp']['times']
+        self.assertEqual(times1, times2)
